3e3111289c232b523007d11d01dd0cb3eeb51cea
kkevi <bora_kim980@naver.com>
2024-06-21 19:14:53 +0900

    init commit : 예제 따라하기 코드

---
+++ app/src/main/java/com/example/introduce/ThirdActivity.kt
@@ -0,0 +1,31 @@
+ package com.example.introduce
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_third)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        val textData = intent.getStringExtra("dataFromFirstActivity")
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.setText(textData)
+
+        val btn_close = findViewById<Button>(R.id.buttonThirdActivity)
+        btn_close.setOnClickListener{
+            finish()
+        }
+    }
+}
