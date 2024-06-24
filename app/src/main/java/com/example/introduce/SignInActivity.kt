1a74b27d9f6bbe835fc7ed02e23584aab4426285
kkevi <bora_kim980@naver.com>
2024-06-24 14:47:03 +0900

    수정 : SignInActivtiy 수정 중...

--- app/src/main/java/com/example/introduce/SignInActivity.kt
+++ app/src/main/java/com/example/introduce/SignInActivity.kt
@@ -6,34 +6,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_signin)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
 
-//        val btn =  findViewById<Button>(R.id.btn1)
-//
-//        btn.setOnClickListener{
-//            val intent = Intent(this, SecondActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        val btn_call3 = findViewById<Button>(R.id.buttonThirdActivity)
-//        btn_call3.setOnClickListener{
+        val loginButton =  findViewById<Button>(R.id.btn_login)
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        val signUpTextView = findViewById<TextView>(R.id.tv_signUp)
+        signUpTextView.setOnClickListener{
 //            val edit_text = findViewById<EditText>(R.id.edit_data)
 //            val textData = edit_text.text.toString()
 //            val intent = Intent(this, ThirdActivity::class.java)
 //            intent.putExtra("dataFromFirstActivity", textData)
 //            startActivity(intent)
-//        }
+        }
     }
 
     // 암시적 인텐트
