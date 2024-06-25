a0ac3280821dc4c78e5abf81238578339f91e106
kkevi <bora_kim980@naver.com>
2024-06-25 17:25:14 +0900

    추가 : 이미지 랜덤으로 처리되도록 로직 추가

--- app/src/main/java/com/example/introduce/HomeActivity.kt
+++ app/src/main/java/com/example/introduce/HomeActivity.kt
@@ -2,8 +2,11 @@ package com.example.introduce
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import java.util.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,24 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val idTextData = intent.getStringExtra("id")
-        println("id >> ${idTextData}")
         val idTextView = findViewById<TextView>(R.id.tv_id)
+        val imageView = findViewById<ImageView>(R.id.iv_profile)
+
         idTextView.text = idTextData
+
+        val images = listOf(
+            R.drawable.image_profile0,
+            R.drawable.image_profile1,
+            R.drawable.image_profile2,
+            R.drawable.image_profile3,
+            R.drawable.image_profile4,
+        )
+
+        val randomNum = Random().nextInt(images.size)
+        println(randomNum)
+
+        val drawable = ResourcesCompat.getDrawable(resources, images[randomNum], null)
+        imageView.setImageDrawable(drawable) // drawble 객체 자체를 가져옴
 
         val btn = findViewById<Button>(R.id.btn_close)
         btn.setOnClickListener{
